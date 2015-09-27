ddbc4499b90f7c4efc30b26ef9121a4b779f1c78
unknown <komysarenkov@ukr.net>
2015-09-28 00:06:18 +0300

    second double

--- src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
@@ -9,7 +9,7 @@ public class TemperatureSeriesAnalysisTest {
     public void testAverage() {
         double[] temperatureSeries = {1.0, -5.0, 1.0, 5.0};
         TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
-        double expResult = 0.0;
+        double expResult = 0.5;
         double actualResult = seriesAnalysis.average();
         
         assertEquals(expResult, actualResult, 0.00001);
