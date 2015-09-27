2120cdc2cc93bcffc44b91e109c371849f7d5d38
unknown <komysarenkov@ukr.net>
2015-09-28 01:05:49 +0300

    six

--- src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
@@ -9,14 +9,14 @@ public class TemperatureSeriesAnalysis {
     
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
         temps = new double[temperatureSeries.length];
-		 for(int i = 0; i < temperatureSeries.length; i++) {
+		for (int i = 0; i < temperatureSeries.length; i++) {
 			temps[i] = temperatureSeries[i];
 		}	
     }
     
     public double average() { 
 		double averageTemp = 0;
-		 for(int i = 0; i < temps.length; i++) {
+		for (int i = 0; i < temps.length; i++) {
 			averageTemp += temps[i];
 		}
         return averageTemp/temps.length;
@@ -25,7 +25,7 @@ public class TemperatureSeriesAnalysis {
     public double deviation(){
 		double averagesqrTemp = 0;
 		
-		for(int i = 0; i < temps.length; i++){
+		for (int i = 0; i < temps.length; i++){
 			averagesqrTemp += Math.pow(temps[i],2);
 		}
         return averagesqrTemp/temps.length-Math.pow(average(),2);
@@ -33,44 +33,44 @@ public class TemperatureSeriesAnalysis {
     
     public double min(){
 		double minimum = bottomTemp;
-		 for(int i = 0; i < temps.length; i++)
-			 if(minimum >= temps[i]) minimum = temps[i];
+		for (int i = 0; i < temps.length; i++)
+			 if (minimum >= temps[i]) minimum = temps[i];
         return minimum;
     }
      
     public double max(){
         double maximum = temps[0];
-		 for(int i = 0; i < temps.length; i++)
-			 if(maximum <= temps[i]) maximum = temps[i];
+		for (int i = 0; i < temps.length; i++)
+			if (maximum <= temps[i]) maximum = temps[i];
         return maximum;
     }
     
     public double findTempClosestToZero(){
         double closestToZero = temps[0];
-		 for(int i = 0; i < temps.length; i++)
-			 if(Math.abs(closestToZero) >= Math.abs(temps[i])) closestToZero = temps[i];
+		for (int i = 0; i < temps.length; i++)
+			 if (Math.abs(closestToZero) >= Math.abs(temps[i])) closestToZero = temps[i];
 		return closestToZero;
     }
     
     public double findTempClosestToValue(double tempValue){
 		double closestTotempValue = temps[0];
 		double minimum = Math.abs(closestTotempValue - temps[0]);
-		 for(int i = 0; i < temps.length; i++)
-			 if(Math.abs(closestTotempValue - temps[i]) < minimum) {
+		for (int i = 0; i < temps.length; i++)
+			if (Math.abs(closestTotempValue - temps[i]) < minimum) {
 				closestTotempValue = temps[i];
 				minimum = Math.abs(closestTotempValue - temps[i]);
 			}
 		return closestTotempValue;
 	}
     
-    public double[] findTempsLessThen(double tempValue){
+    public double [] findTempsLessThen(double tempValue){
 		int count = 0;
-		 for(int i = 0; i < temps.length; i++)
-			 if(temps[i] < tempValue) count++;
-		double[] TempLessThen = new double [count];
+		for (int i = 0; i < temps.length; i++)
+			 if (temps[i] < tempValue) count++;
+		double [] TempLessThen = new double [count];
 		count = 0;
-		 for(int i = 0; i < temps.length; i++){
-			 if(temps[i] < tempValue) {
+		for (int i = 0; i < temps.length; i++){
+			if(temps[i] < tempValue) {
 				count++;
 				TempLessThen[count] = temps[i];
 			 }
@@ -78,14 +78,14 @@ public class TemperatureSeriesAnalysis {
         return TempLessThen;
     }
     
-    public double[] findTempsGreaterThen(double tempValue){
+    public double [] findTempsGreaterThen(double tempValue){
 		int count = 0;
-		for(int i = 0; i < temps.length; i++)
-			if(temps[i] > tempValue) count++;
-		double[] TempGreaterThen = new double [count];
+		for (int i = 0; i < temps.length; i++)
+			if (temps[i] > tempValue) count++;
+		double [] TempGreaterThen = new double [count];
 		count = 0;
-		 for(int i = 0; i < temps.length; i++){
-			 if(temps[i] > tempValue) {
+		 for (int i = 0; i < temps.length; i++){
+			 if (temps[i] > tempValue) {
 				count++;
 				TempGreaterThen[count] = temps[i];
 			 } 
