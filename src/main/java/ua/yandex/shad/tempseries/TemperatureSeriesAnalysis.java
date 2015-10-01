2c76ba1e5550310b660a01b91e3d5c93f89505bc
unknown <komysarenkov@ukr.net>
2015-10-01 16:52:53 +0300

    53

--- src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
@@ -167,7 +167,7 @@ public class TemperatureSeriesAnalysis {
 	
 	
     public TempSummaryStatistics summaryStatistics() {
-		static summaryStat = new TempSummaryStatistics();
+		TempSummaryStatistics summaryStat = new TempSummaryStatistics();
 		if (temps.length == 0) { 
 			throw new IllegalArgumentException();
 		}
